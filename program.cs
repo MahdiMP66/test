576afc1f10d35b6bcb9b60b782f7642ab84c7450
mahdimp <madanipour66@gmail.com>
2022-06-23 17:54:59 +0430

    added pow3 method

--- program.cs
+++ program.cs
@@ -5,12 +5,17 @@ namespace ConsoleApp1
 
     class Program
     {
+        public static void pow3(int a)
+        {
+            Console.WriteLine(a*a*a);
+        }
         public static void pow2(int a)
         {
             Console.WriteLine(a*a);
         }
         static void Main(string[] args)
         {
+            pow3(5);
             pow2(5);
         }
     }
